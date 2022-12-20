48625f1a31e93f8c23eb1bb78c08ea40b2254948
hoanglh1 <git@gitlab.citigo.com.vn:kfinance/base.git>
2022-12-20 17:06:55 +0700

    init

--- api/main.go
+++ api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("haha")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/prometheus", gin.WrapH(promhttp.Handler()))
+	if err := r.Run(":1234"); err != nil {
+		panic(err)
+	}
 }
