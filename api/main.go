81a29d08ef6c00d43ea0c4094dea935eaf974c3e
hoanglh1 <git@gitlab.citigo.com.vn:kfinance/base.git>
2022-12-20 17:28:16 +0700

    init

--- api/main.go
+++ api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"api/src/present/middleware"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 )
 
 func main() {
+	middleware.RecordMetrics()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
